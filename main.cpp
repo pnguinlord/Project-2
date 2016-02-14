bf130c0d9eed68b50227c7900204419f2121b082
akaduwela <asithank@gmail.com>
2016-02-14 14:35:00 -0800

    frequency and stored set individually, heapify doesn't do anything

--- main.cpp
+++ main.cpp
@@ -25,40 +25,120 @@ void heapify(treeNode input[])
         }
         if (i*2+2>=size){
           if(input[j].freq>input[j*2+1].freq){
+            
+            //temp node for swap
+          	treeNode temp;
+          	temp.freq = input[j].freq;
+          	temp.stored = input[j].stored;
+
+          	input[j].freq = input[j*2+1].freq;
+          	input[j].stored = input[j*2+1].stored;
+          	
+          	input[j*2+1].freq=temp.freq;
+          	input[j*2+1].stored=temp.stored;
+
+/*
             treeNode temp=input[j];
             input[j]=input[j*2+1];
             input[j*2+1]=temp;
-            
+*/
           }
           satisfied=1;
         }
         else{
-          if((input[j*2+1].freq)<(input[j].freq)&&(input[j*2+2].freq<input[j].freq)){
-          	if((input[j*2+1].freq)<(input[j*2+2].freq)) {
+          if((input[j*2+1].freq)<(input[j].freq)&&(input[j*2+2].freq<input[j].freq)){ // if both left and right children are less than parent
+          	if((input[j*2+1].freq)<(input[j*2+2].freq)) { // if left child is less than right, swap left with parent
+            
+            //temp node for swap
+          	treeNode temp;
+          	temp.freq = input[j].freq;
+          	temp.stored = input[j].stored;
+
+          	input[j].freq = input[j*2+1].freq;
+          	input[j].stored = input[j*2+1].stored;
+          	
+          	input[j*2+1].freq=temp.freq;
+          	input[j*2+1].stored=temp.stored;
+
+            j=j*2+1;
+
+/*
             treeNode temp=input[j];
             input[j]=input[j*2+1];
             input[j*2+1]=temp;
             j=j*2+1;
+*/
+
           }
-          else{
+          else{ // if right child is less than left, swap right with parent
+
+            //temp node for swap
+          	treeNode temp;
+          	temp.freq = input[j].freq;
+          	temp.stored = input[j].stored;
+
+          	input[j].freq = input[j*2+2].freq;
+          	input[j].stored = input[j*2+2].stored;
+          	
+          	input[j*2+2].freq=temp.freq;
+          	input[j*2+2].stored=temp.stored;
+
+			j=j*2+2;
+          	
+          	/*
             treeNode temp=input[j];
             input[j]=input[j*2+2];
             input[j*2+2]=temp;
             j=j*2+2;
+            */
           }
         }
-        else if((input[j*2+1].freq)<(input[j].freq) && (input[j*2+2].freq>input[j].freq))
-        {   treeNode temp=input[j];
+        else if((input[j*2+1].freq)<(input[j].freq) && (input[j*2+2].freq>input[j].freq)) {   // if left<parent and right>parent, swap left with parent
+
+            //temp node for swap
+          	treeNode temp;
+          	temp.freq = input[j].freq;
+          	temp.stored = input[j].stored;
+
+          	input[j].freq = input[j*2+1].freq;
+          	input[j].stored = input[j*2+1].stored;
+          	
+          	input[j*2+1].freq=temp.freq;
+          	input[j*2+1].stored=temp.stored;
+
+            j=j*2+1;
+
+
+/*
+        	treeNode temp=input[j];
             input[j]=input[j*2+1];
             input[j*2+1]=temp;
             j=j*2+1;
+  */
         }
-       else if((input[j*2+1].freq)>(input[j].freq)&&(input[j*2+2].freq<input[j].freq))
+       else if((input[j*2+1].freq)>(input[j].freq)&&(input[j*2+2].freq<input[j].freq)) // if left>parent and right<parent, swap right with parent
           {
+
+            //temp node for swap
+          	treeNode temp;
+          	temp.freq = input[j].freq;
+          	temp.stored = input[j].stored;
+
+          	input[j].freq = input[j*2+2].freq;
+          	input[j].stored = input[j*2+2].stored;
+          	
+          	input[j*2+2].freq=temp.freq;
+          	input[j*2+2].stored=temp.stored;
+
+			j=j*2+2;
+
+          	/*
             treeNode temp=input[j];
             input[j]=input[j*2+2];
             input[j*2+2]=temp;
             j=j*2+2;
+            */
+
           }
         else{
           satisfied=1;
@@ -153,10 +233,10 @@ int main()
       }
     }
     
-  //heapify(minheap);
+  heapify(minheap);
   for(int i=0; i<ARRAY_SIZE(minheap);i++)
-    {cout<<minheap[i].freq;
-     cout<<minheap[i].stored;
+    {cout<<minheap[i].freq << " ";
+     cout<<minheap[i].stored << "\n";
     }
 
   return 0;
