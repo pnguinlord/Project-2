ca98d967faa4e319c52a49b833c63d94fb15f73d
pnguinlord <richardwei0002@gmail.com>
2016-02-14 11:27:38 -0800

    heapify written
    logic sound i think

--- main.cpp
+++ main.cpp
@@ -13,41 +13,97 @@ struct treeNode{
   char stored;
 };
 
-void heapify(treeNode input[]){
-    for(int i=0;i<ARRAY_SIZE(input);i++){
-    	int j=i;
+void heapify(treeNode input[])
+{
+  int size=ARRAY_SIZE(input);
+  for(int i=size/2-1;i>=0;i--){ //verify that the minus one is correct
+      int j=i;
+      int satisfied=0;
+      while (satisfied==0){
+        if(j>size/2){
+          satisfied=1;
+        }
+        if (i*2+2>=size){
+          if(input[j].freq>input[j*2+1].freq){
+            treeNode temp=input[j];
+            input[j]=input[j*2+1];
+            input[j*2+1]=temp;
+            
+          }
+          satisfied=1;
+        }
+        else{
+          if((input[j*2+1].freq)<(input[j].freq)&&(input[j*2+2].freq<input[j].freq))
+          {if((input[j*2+1])<input[j*2+2]){
+            treeNode temp=input[j];
+            input[j]=input[j*2+1];
+            input[j*2+1]=temp;
+            j=j*2+1;
+          }
+          else{
+            treeNode temp=input[j];
+            input[j]=input[j*2+2];
+            input[j*2+2]=temp;
+            j=j*2+2;
+          }
+        }
+        else if((input[j*2+1].freq)<(input[j].freq)&&(input[j*2+2].freq>input[j].freq))
+        {   treeNode temp=input[j];
+            input[j]=input[j*2+1];
+            input[j*2+1]=temp;
+            j=j*2+1;
 
-    	while ((input[j]))
+        }
+         
+       else if if((input[j*2+1].freq)>(input[j].freq)&&(input[j*2+2].freq<input[j].freq))
+          {
+            treeNode temp=input[j];
+            input[j]=input[j*2+2];
+            input[j*2+2]=temp;
+            j=j*2+2;
+
+          }
+        else{
+          satisfied=1;
+        }
+
+
+          }
+        }
 
 
 
 
 
-      if((input[i*2+1].freq>input[i*2+2].freq)&&(input[i*2+1].freq<input[i].freq))
-	{
-	  treeNode temp=input[i];
-	  input[i]=input[i*2+1];
-	  input[i*2+1]=temp;
-	}
-      else if((input[i*2+2].freq>input[i*2+1].freq)&&(input[i*2+2].freq<input[i].freq))
-	{
-	  treeNode temp=input[i];
-	  input[i]=input[i*2+2];
-	  input[i*2+2]=temp;
-	}
-      else if((input[i*2+1].freq==input[i*2+2].freq)&&(input[i*2+1].freq<input[i].freq))
-	{
-	  treeNode temp=input[i];
-	  input[i]=input[i*2+2];
-	  input[i*2+2]=temp;
-	}
-      
+      }
     }
-   
+
+
+
+    /*  if((input[i*2+1].freq>input[i*2+2].freq)&&(input[i*2+1].freq<input[i].freq))
+  {
+    treeNode temp=input[i];
+    input[i]=input[i*2+1];
+    input[i*2+1]=temp;
+  }
+      else if((input[i*2+2].freq>input[i*2+1].freq)&&(input[i*2+2].freq<input[i].freq))
+  {
+    treeNode temp=input[i];
+    input[i]=input[i*2+2];
+    input[i*2+2]=temp;
+  }
+      else if((input[i*2+1].freq==input[i*2+2].freq)&&(input[i*2+1].freq<input[i].freq))
+  {
+    treeNode temp=input[i];
+    input[i]=input[i*2+2];
+    input[i*2+2]=temp;
+  }
+      
+    
 }
       
   
-
+*/
 
 int main()
 {
@@ -61,22 +117,22 @@ int main()
   for (int i=0; i<str.length();i++) //iterate through the string 
     {
       if(97<=str[i]&&str[i]<=122) //if the char is a-z
-	{
-	if (freq[str[i]-'a']==0){
-	  count++; // counts unique characters
-	  
-	  }
-	freq[str[i]-'a']++;//increment respective index 
-	
+  {
+  if (freq[str[i]-'a']==0){
+    count++; // counts unique characters
+    
+    }
+  freq[str[i]-'a']++;//increment respective index 
+  
       }
       else if(str[i]==32)
-	{
-	  if( freq[26]==0){
-	      count++;
-	      
-	  }
-	freq[26]++; // if space detected, increment its index
-	
+  {
+    if( freq[26]==0){
+        count++;
+        
+    }
+  freq[26]++; // if space detected, increment its index
+  
       
       }
     }
@@ -93,7 +149,7 @@ int main()
     
     if (freq[i]!=0)
       {
-	// cout<<freq[i];
+  // cout<<freq[i];
     minheap[j].freq=freq[i];
     if (freq[i]!=32){
       minheap[j].stored=char(i+'a');
