322b962f37c3d3ab0f3614048b80e0bdc0cbf9ef
Richard Wei <rwei@csil.cs.ucsb.edu>
2016-02-14 22:44:28 -0800

    compiles, encode.h updated

--- encode.h
+++ encode.h
@@ -10,5 +10,6 @@ treeNode* deleteMin(treeNode* tree, int size);
 void percolateDown(treeNode*tree, int size);
 void insertnode(treeNode* insert[], treeNode*);
 void percolateUp(treeNode*tree, int size);
+void makeTrie(treeNode* minheap[], int size){
 
 #endif
